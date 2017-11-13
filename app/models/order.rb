06cda5c28a0d156fc457d588fc2200bd2b12699f
Louis Mathe <mathe.louis@gmail.com>
2017-11-13 18:38:27 +0100

    fix quantity in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true
-  validates :quantity, numericality: { greater_than: g }
+  validates :quantity, numericality: { greater_than: 0 }
 end

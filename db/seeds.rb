dcbbe8323863ded408f1cca80763322ca4c0d9d6
Louis Mathe <mathe.louis@gmail.com>
2017-11-14 11:01:04 +0100

    seed 3x[users,farms,products,orders]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,66 @@
-# ## Generate Seeds
-
 # 0. Clean all DB
-Product.destroy_all
 Order.destroy_all
+Product.destroy_all
 Farm.destroy_all
 User.destroy_all
 
-
-# # 1. Create user as farmer
-# john = User.new(email: "john@gmail.com", password: "123456789")
-#
-#
-#
-#
-# # 2. Create farm
-# johnfarm = Farm.new(name:"La ferme de John", address: "Bordeaux")
-#
-# # 3. Assign user to farm
-# johnfarm.user = john
-#
-# # 4. Create user as customer
-# serge = User.new(email: "Serge@gmail.com", password: "123456789")
-#
-# # 5. Create a product
-# pomme = Product.new(name: "pomme", price: 5)
-#
-# # 6. Assign product to farm
-# pomme.farm = johnfarm
-#
-# # 7. Create an order with customer + product
-# firstorder = Order.new(quantity: 1)
-# firstorder.product = pomme
-# firstorder.user = serge
-#
-# john.save
-# johnfarm.save
-# serge.save
-# pomme.save
-# firstorder.save
-
-
-
 # 1. Create user as farmer
-john = User.create!(email: "john@gmail.com", password: "123456789")
+john = User.new(email: 'john@gmail.com', password: '123456789')
+paul = User.new(email: 'paul@gmail.com', password: '123456789')
+georges = User.new(email: 'georges@gmail.com', password: '123456789')
 
 # 2. Create farm
-johnfarm = Farm.create!(name:"La ferme de John", address: "Bordeaux")
+johnfarm = Farm.new(name: 'La ferme de John', address: 'Bordeaux')
+paulfarm = Farm.new(name: 'La ferme de Paul', address: 'Talence')
+georgesfarm = Farm.new(name: 'La ferme de Georges', address: 'Pessac')
 
 # 3. Assign user to farm
 johnfarm.user = john
+paulfarm.user = paul
+georgesfarm.user = georges
 
 # 4. Create user as customer
-serge = User.create!(email: "Serge@gmail.com", password: "123456789")
+serge = User.new(email: 'serge@gmail.com', password: '123456789')
+quentin = User.new(email: 'quentin@gmail.com', password: '123456789')
+louis = User.new(email: 'louis@gmail.com', password: '123456789')
 
 # 5. Create a product
-pomme = Product.create!(name: "pomme", price: 5)
+pomme = Product.new(name: 'pomme', price: 5)
+peche = Product.new(name: 'peche', price: 1)
+poire = Product.new(name: 'poire', price: 11)
 
 # 6. Assign product to farm
 pomme.farm = johnfarm
+poire.farm = paulfarm
+peche.farm = georgesfarm
 
 # 7. Create an order with customer + product
-firstorder = Order.create!(quantity: 1)
-firstorder.product = pomme
-firstorder.user = serge
+sergeorder = Order.new(quantity: 1)
+sergeorder.product = pomme
+sergeorder.user = serge
+
+john.save
+johnfarm.save
+serge.save
+pomme.save
+sergeorder.save
+#################
+quentinorder = Order.new(quantity: 1)
+quentinorder.product = poire
+quentinorder.user = quentin
+
+paul.save
+paulfarm.save
+quentin.save
+poire.save
+quentinorder.save
+#################
+louisorder = Order.new(quantity: 1)
+louisorder.product = peche
+louisorder.user = louis
+
+georges.save
+georgesfarm.save
+louis.save
+peche.save
+louisorder.save

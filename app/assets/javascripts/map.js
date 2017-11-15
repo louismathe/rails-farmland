4d8860bad3d5f0f500f7c9731609841c7d088d5a
Louis Mathe <mathe.louis@gmail.com>
2017-11-15 14:31:40 +0100

    comment js for deploy to heroku

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,25 +1,25 @@
-const address = document.getElementById("address");
-const mapContainer = document.getElementById('map');
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.value}`)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data)
-    const latitude = (data.results[0].geometry.location.lat)
-    const longitude = (data.results[0].geometry.location.lng)
-
-    let addressFound = {lat: latitude, lng: longitude };
-    console.log(addressFound)
-    let map = new google.maps.Map(mapContainer, {
-      zoom: 12,
-      center: addressFound
-    });
-    console.log(document.getElementById('map'))
-
-    let marker = new google.maps.Marker({
-      position: addressFound,
-      map: map
-    });
-  });
-});
+// const address = document.getElementById("address");
+// const mapContainer = document.getElementById('map');
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.value}`)
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data)
+//     const latitude = (data.results[0].geometry.location.lat)
+//     const longitude = (data.results[0].geometry.location.lng)
+//
+//     let addressFound = {lat: latitude, lng: longitude };
+//     console.log(addressFound)
+//     let map = new google.maps.Map(mapContainer, {
+//       zoom: 12,
+//       center: addressFound
+//     });
+//     console.log(document.getElementById('map'))
+//
+//     let marker = new google.maps.Marker({
+//       position: addressFound,
+//       map: map
+//     });
+//   });
+// });

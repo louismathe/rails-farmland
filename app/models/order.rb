ae0bae73d702d32dbdbf569a44a21f9403732bce
serge cleoron <sc21417@gmail.com>
2017-11-17 14:17:08 +0100

    validation on selectinput for rating (1 to 5 only)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,13 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  ORDER_RATING = [1, 2, 3, 4, 5]
+
   validates :quantity, presence: true
   validates :quantity, numericality: { greater_than: 0 }
-  validates :rating, inclusion: { in: (0..5), on: :update }
+  validates :rating, inclusion: { in: ORDER_RATING, on: :update }
+
+
 
   # def rated?
   #   created_at != updated_at
